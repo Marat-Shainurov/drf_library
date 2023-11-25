b99a1d8c654de5b281f9559f319bd87b7ae517fb
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-25 23:31:04 +0400

    configures admin site interface for the Book and CustomUser models

--- library/admin.py
+++ library/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from library.models import Book
+
+
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    list_display = ('id', 'title', 'author', 'publish_year', 'ISBN')
+    list_filter = ('author', 'publish_year')
+    search_fields = ('id', 'title', 'ISBN')
