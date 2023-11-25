f165f8eb34a8a12ffb4feec131dd0492204ddb94
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-25 23:22:54 +0400

    installs and configures djangorestframework-simplejwt authorization, adds the getting token routes to users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,17 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import CreateUserAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('users/register', CreateUserAPIView.as_view(), name='register_user')
+    path('users/register', CreateUserAPIView.as_view(), name='register_user'),
+
+    # drf-simplejwt
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
