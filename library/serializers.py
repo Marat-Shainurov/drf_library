ad8a5a68cd1cd6d4553f9a7b653a93f7f3746d08
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-26 00:01:25 +0400

    updates the CreateBookAPIView view with request body/response schemas, adds the BookCreateSerializer serializer

--- library/serializers.py
+++ library/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from library.models import Book
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BookCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', 'author', 'ISBN', 'publish_year')
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'author', 'ISBN', 'publish_year')
