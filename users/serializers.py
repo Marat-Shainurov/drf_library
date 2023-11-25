d44df041d60f27ea412c54bb4f0d148d736b7863
Marat-Shainurov <m_shainurov@mail.ru>
2023-11-25 23:59:52 +0400

    updates the CustomUserCreateSerializer with the password field and validate_password method

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from users.models import CustomUser
@@ -6,10 +7,14 @@ from users.models import CustomUser
 class CustomUserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'password')
+
+    @staticmethod
+    def validate_password(value: str) -> str:
+        return make_password(value)
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'registration_date')
+        fields = ('id', 'username', 'email', 'registration_date')
